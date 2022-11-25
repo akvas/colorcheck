5960820425eb4836c3920ab4a1c760d5c9e2d47c
Andreas Kvas <andreas@kvas.at>
2022-11-25 22:03:33 +0100

    added function to equalized colormaps

--- colorcheck/cmap.py
+++ colorcheck/cmap.py
@@ -105,3 +105,74 @@ def lightness_gradient(cmap, samples=256):
     dE = np.sum(np.gradient(lab, axis=0)**2, axis=1)
 
     return dE, np.cumsum(dE) 
+
+
+def equalize_cmap(cmap, metric='lightness'):
+    """
+    Equalize the percetual gradient of a colormap.
+
+    Parameters
+    ----------
+    cmap : colormap-like
+        Colormap-like object.
+    metric : str
+        The metric used to linearize the perception. One of ('lightness', 'CIE76').
+
+    Returns
+    -------
+    cmap_equalized : colormap-like
+        The equalized colormap.
+    """
+    x = np.linspace(0, 1, cmap.N)
+    colors = cmap(x)
+    rgb = colors[np.newaxis, :, :3] 
+    lab = colorspacious.cspace_converter("sRGB1", "CAM02-UCS")(rgb)[0, :, :]
+
+    if metric == 'lightness':
+        dE = np.cumsum(np.abs(np.gradient(lab[:, 0])))
+    elif metric == 'CIE76':
+        dE = np.cumsum(np.sqrt(np.sum(np.gradient(lab, axis=0)**2, axis=1)))
+    else: 
+        raise ValueError('Metric must be one of "lightness" or "CIE76"')
+
+    x_interp = np.interp(dE, np.linspace(dE[0], dE[-1], cmap.N), x)
+
+    if cmap.name.endswith('_r'):
+        output_name = cmap.name[0:-2] + '_equalized_r'
+    else:
+        output_name = cmap.name + '_equalized'
+
+    rgb_equalized = [(xk, ck) for xk, ck in zip(x_interp, colors)]
+
+    return matplotlib.colors.LinearSegmentedColormap.from_list(output_name, rgb_equalized)
+
+
+def cmapshow(cmap, ax=None, width=640, height=80, orientation='horizontal'):
+    """
+    Convenience function for displaying colormaps in an existing axes instance.
+
+    Parameters
+    ----------
+    cmap : colormap-like
+        Colormap-like object.
+    ax : Axes object or None
+        Axes into which the colormap is drawn. If None, the currently active axes are used.
+    width : int
+        Width of the image array in pixels.
+    height : int
+        Height of the image array in pixels.
+    orientation : str
+        Orientiation of the colormap gradient. One of 'horizonal' (default) or 'vertical'.
+
+    Returns
+    -------
+    im : mappable
+        The mappable returned by imshow.
+    """
+    if ax is None:
+        ax = matplotlib.pyplot.gca()
+
+    img = cmap_rgba(cmap, width, height, orientation)
+    im = ax.imshow(img)
+
+    return im
